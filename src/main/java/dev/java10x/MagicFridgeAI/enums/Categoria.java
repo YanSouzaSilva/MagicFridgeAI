dd464bc2b59560ac04550c54a232c0d4719b916c
Yan Souza Silva <yanafonso@gmail.com>
2026-05-08 09:37:06 -0300

    feat: Cria enum com a listagem da categoria dos alimentos

---
+++ src/main/java/dev/java10x/MagicFridgeAI/enums/Categoria.java
@@ -0,0 +1,15 @@
+package dev.java10x.MagicFridgeAI.enums;
+
+public enum Categoria {
+        CARNE,
+        FRUTA,
+        LEGUME,
+        VERDURA,
+        GRAO,
+        MASSA,
+        LATICINIO,
+        TEMPERO,
+        OLEO_E_GORDURA,
+        BEBIDA,
+        OUTROS
+}
