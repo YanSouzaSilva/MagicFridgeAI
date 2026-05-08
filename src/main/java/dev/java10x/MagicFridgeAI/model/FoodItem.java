6196b80ade52e696b0cc9af5da2b8f705540a366
Yan Souza Silva <yanafonso@gmail.com>
2026-05-08 11:50:21 -0300

    fix: adiciona restrição nullable=false ao campo nome

--- src/main/java/dev/java10x/MagicFridgeAI/model/FoodItem.java
+++ src/main/java/dev/java10x/MagicFridgeAI/model/FoodItem.java
@@ -15,6 +15,7 @@ public class FoodItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
     @Enumerated(EnumType.STRING)
@@ -23,4 +24,44 @@ public class FoodItem {
     private Integer quantidade;
 
     private LocalDateTime validade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDateTime getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDateTime validade) {
+        this.validade = validade;
+    }
 }
